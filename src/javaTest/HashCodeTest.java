b61a691b6e9aaf1608fdaa3601d1196e4360f5f6
梁新建 <liangxinjian@cmos.chinamobile.com>
2018-12-24 19:33:38 +0800

    xiugai

--- src/javaTest/HashCodeTest.java
+++ src/javaTest/HashCodeTest.java
@@ -8,10 +8,11 @@ public class HashCodeTest {
 		System.out.println("s1=====>" + s1.hashCode() + '\n' +"s2=====>" + s2.hashCode());
 		String s3 = "abcd";
 		String s4 = "abcd";
+		String s5 = "HelloWorld";
 		System.out.println("s3=====>" + s3.hashCode() + '\n' +"s4=====>" + s4.hashCode());
 		System.out.println("s3=====>" + System.identityHashCode(s3) + '\n' +"s4=====>" + System.identityHashCode(s4));
 		System.out.println("s1=====>" + System.identityHashCode(s1) + '\n' +"s2=====>" + System.identityHashCode(s2));
-		
+		System.out.println("欢迎您" + s5);
 	}
 
 }
